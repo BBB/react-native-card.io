c43ae5138480ce5a5b9a79e8eafaba721f951df9
Ollie Relph <ollie@relph.me>
2015-10-17 17:44:03 +0100

    Add an index.js

--- ios.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./lib/CardIO.ios');
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import CardIOView from './CardIO.ios';
+import * as constants from './constants';
+
+export default CardIOView;
+export const CardIOConstants = constants;
